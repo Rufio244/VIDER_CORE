ab11b56ce2bb5a455f301b72a2c723b3b374ce22
Rufio244 <thanvas9991@gmail.com>
2026-06-18 23:45:49 +0700

    Update storage.py

--- memory/storage.py
+++ memory/storage.py
@@ -2,3 +2,30 @@
     "knowledge": [],
     "projects": []
 }
+import json
+
+
+FILE="memory.json"
+
+
+def save(topic,data):
+
+    with open(FILE,"r",encoding="utf-8") as f:
+        mem=json.load(f)
+
+
+    mem["knowledge"].append(
+        {
+            "topic":topic,
+            "data":data
+        }
+    )
+
+
+    with open(FILE,"w",encoding="utf-8") as f:
+        json.dump(
+            mem,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
