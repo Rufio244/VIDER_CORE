932480ad5e7e1402c14187e58f5bd550973164f8
Rufio244 <thanvas9991@gmail.com>
2026-06-18 23:47:26 +0700

    Implement FastAPI server with command handling

--- api/server.py
+++ api/server.py
@@ -1,1 +1,37 @@
+from fastapi import FastAPI
+from brain.chani import think
+from brain.planner import create_plan
+from memory.storage import save
 
+
+app=FastAPI()
+
+
+@app.get("/")
+def home():
+
+    return {
+        "system":"VIDER CORE",
+        "status":"online"
+    }
+
+
+@app.post("/command")
+def command(data:dict):
+
+    text=data["command"]
+
+    brain=think(text)
+
+    plan=create_plan(text)
+
+    save(
+        "command",
+        text
+    )
+
+
+    return {
+        "brain":brain,
+        "plan":plan
+    }
